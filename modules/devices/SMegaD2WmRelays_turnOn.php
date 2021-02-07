11ea3e4288c8731c13201838d678e2815958ea86
Паша <odinwolk@gmail.com>
2021-02-07 17:33:13 +0300

    Create SMegaD2WmRelays_turnOn.php

---
+++ modules/devices/SMegaD2WmRelays_turnOn.php
@@ -0,0 +1,24 @@
+<?php
+$this->setProperty('status', 1);
+
+$ot = $this->object_title;
+$blocked_timeout = $ot.'_lightOffTimer';
+$timeout = $this->getProperty('timeout'); //60 * 15; 15 мин timeout
+$ipAddress = $this->getProperty('ipAddress');
+$Password = $this->getProperty('Password');
+$Port = $this->getProperty('Port');
+$ch = $this->getProperty('ch');
+$status = $this->getProperty('status');
+$Address = $this->getProperty('bAddress');
+
+//Добавляем таймер на автовыключение света
+if ($this->getProperty('autoMode') == 1) {
+    clearTimeOut($blocked_timeout);
+    setTimeOut($blocked_timeout,'cm("'.$ot.'.turnOff");',60 * $timeout);
+} else {
+    $this->setProperty('status', 1);
+}
+
+//http://192.168.10.101/sec/?cmd=8A:0&addr=312538000000
+//Включение канала
+file_get_contents("http://".$ipAddress."/".$Password."/?cmd=".$Port.$ch.":1&addr=".$Address);
