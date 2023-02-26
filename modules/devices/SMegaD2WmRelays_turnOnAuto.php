ca256eecb18533765c2cabdbd83b05a79c84d0d9
Паша <odinwolk@gmail.com>
2023-02-26 04:46:38 +0300

    Update SMegaD2WmRelays_turnOnAuto.php

--- modules/devices/SMegaD2WmRelays_turnOnAuto.php
+++ modules/devices/SMegaD2WmRelays_turnOnAuto.php
@@ -1,5 +1,4 @@
 <?php
-
 $ot = $this->object_title;
 $blocked_timeout = $ot.'_lightOffTimer';
 $timeout = $this->getProperty('timeout'); //60 * 15; 15 мин timeout
@@ -11,12 +10,9 @@ $status = $this->getProperty('status');
 $Address = $this->getProperty('bAddress');
 
 //Добавляем таймер на автовыключение света
-//if ($this->getProperty('autoMode') == 1) {
     clearTimeOut($blocked_timeout);
     setTimeOut($blocked_timeout,'cm("'.$ot.'.turnOff");',60 * $timeout);
-//} else {
     $this->setProperty('status', 1);
-//}
 
 //http://192.168.10.101/sec/?cmd=8A:0&addr=312538000000
 //Включение канала
